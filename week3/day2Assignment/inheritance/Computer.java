3f662bbd4cda0d5c53391d099409774628512cac
RaghuRavichandran <121370574+RaghuRavichandran@users.noreply.github.com>
2023-01-27 20:11:01 +0530

    Add files via upload

---
+++ week3/day2Assignment/inheritance/Computer.java
@@ -0,0 +1,18 @@
+package week3.day2Assignment;
+
+public class Computer {
+
+	public void computerModel() {
+		System.out.println("Lenovo");
+	}
+}
+	
+//Package   :org.system
+//Class        :Computer
+//Methods   :computerModel()
+
+//Class        :Desktop
+//Methods   :desktopSize()
+
+//Description:
+//create above 2 class and call all your class methods into the Desktop using single inheritance.
