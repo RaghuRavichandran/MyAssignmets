65faf257ff8223e700325809544e9db8b24d29f2
RaghuRavichandran <121370574+RaghuRavichandran@users.noreply.github.com>
2023-01-27 20:18:57 +0530

    Add files via upload

---
+++ week3/day2Assignment/Overriding/AxisBank.java
@@ -0,0 +1,13 @@
+package week3.day2Assignment;
+
+public class AxisBank extends BankInfo{
+
+public void deposit() {
+	System.out.println("Deposit Amount");
+	}
+public static void main(String[] args) {
+	AxisBank obj=new AxisBank();
+	obj.deposit();
+	
+	}
+}
